21d9247e36fbafd7dd6127abad0e27bb76bb19a6
Maurice Lam <me@mauricelam.com>
2013-11-15 02:58:46 -0600

    Some bootstrapping stuff

--- src/com/mauricelam/Savier/SaveActivity.java
+++ src/com/mauricelam/Savier/SaveActivity.java
@@ -2,14 +2,53 @@ package com.mauricelam.Savier;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
 
 public class SaveActivity extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+
+        // Set up the grid
+        GridView gridview = (GridView) findViewById(R.id.goalGrid);
+        gridview.setAdapter(new GoalGridAdapter(this));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                Toast.makeText(SaveActivity.this, "Item number " + position, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.mainmenu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.action_add_goal:
+                Toast.makeText(this, "There is no spoon", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
+
+
