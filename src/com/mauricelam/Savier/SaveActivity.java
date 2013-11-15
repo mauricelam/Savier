981db5f550e06d3bf372fce39ae89859f343098a
Maurice Lam <me@mauricelam.com>
2013-11-14 23:42:01 -0600

    Initial commit

---
+++ src/com/mauricelam/Savier/SaveActivity.java
@@ -0,0 +1,15 @@
+package com.mauricelam.Savier;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SaveActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
