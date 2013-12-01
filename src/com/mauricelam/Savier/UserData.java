f02fcce8d85d4f3c297ecc0f645354d0d740df16
Pulkit Budhiraja <budhi15@gmail.com>
2013-11-30 19:10:14 -0600

    Pulkit's changes

---
+++ src/com/mauricelam/Savier/UserData.java
@@ -0,0 +1,39 @@
+package com.mauricelam.Savier;
+
+
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class UserData implements Parcelable {
+	String fName;
+	String lName;
+	Address ShippingAddress;
+	Address BillingAddress;
+	Card CheckingCard;
+	
+	UserData()
+	{
+		fName = "First Name";
+		lName = "Last Name";
+		ShippingAddress = new Address();
+		BillingAddress = new Address();
+		CheckingCard = new Card(fName, lName);
+	}
+
+	@Override
+	public int describeContents() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void writeToParcel(Parcel dest, int flags) {
+		dest.writeString(fName);
+		dest.writeString(lName);
+		dest.write
+		
+	}
+	
+	
+}
